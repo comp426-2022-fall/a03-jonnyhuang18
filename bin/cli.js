9af636f6aec740c38ff8718eaad53615ec95427a
Jonny Huang <jsuwen@ad.unc.edu>
2022-11-30 12:03:09 -0500

    json

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { roll } from "../lib/roll";
 import minimist from "minimist";
+import { roll } from "../lib/roll";
 
 const args = minimist(process.argv.slice(2));
 
@@ -8,15 +8,8 @@ var sides = 6;
 var dice = 2;
 var rolls = 1;
 
-if(args.sides > 0){
-    sides = args.sides
-}
-if(args.dice > 0){
-    dice = args.dice
-}
-if(args.rolls > 0){
-    rolls = args.rolls
-}
+export function main(){
+    console.log(JSON.stringify(roll(args.sides,args.dice,args.rolls)));
+};
 
-console.log(JSON.stringify(roll(sides, dice, rolls)));
-process.exit(0);
+main();
