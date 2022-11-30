7f5f22fe02c0012f67975d7eb7134a34578008bf
Jonny Huang <jsuwen@ad.unc.edu>
2022-11-30 11:00:21 -0500

    read in args

--- bin/cli.js
+++ bin/cli.js
@@ -3,3 +3,11 @@ import { roll } from "/lib/roll.js"
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
+
+var sides = 6;
+var dice = 2;
+var rolls = 1;
+
+sides = args.sides;
+dice = args.dice;
+rolls = args.rolls;
